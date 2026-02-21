43c7d2e47da9a4470fb42dee7f273f7adf957eb5
Mateo Lara <mateodev99@gmail.com>
2026-02-21 12:12:57 +0100

    ci: configure github pages deployment pipeline with vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pokeTactics/', // This MUST match the GitHub repository name
   plugins: [
     tailwindcss(),
     react()
